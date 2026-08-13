4d705797ab8b2eea3bbcfc713a64a431322a6b31
Manu Paul <manupaul535@gmail.com>
2026-08-13 13:32:50 +0530

    initial setup

---
+++ videoworker/src/main/java/com/miniyoutube/videoworker/VideoworkerApplication.java
@@ -0,0 +1,13 @@
+package com.miniyoutube.videoworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoworkerApplication.class, args);
+	}
+
+}
---
+++ videoworker/src/test/java/com/miniyoutube/videoworker/VideoworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniyoutube.videoworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
