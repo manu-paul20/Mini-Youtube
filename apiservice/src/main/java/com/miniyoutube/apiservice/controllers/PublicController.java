4a71171bcb980e70dfe44b0e52d4ba345baab6d1
Manu Paul <manupaul535@gmail.com>
2026-08-14 11:06:18 +0530

    added a health check controller

---
+++ apiservice/src/main/java/com/miniyoutube/apiservice/controllers/PublicController.java
@@ -0,0 +1,13 @@
+package com.miniyoutube.apiservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    @GetMapping("/health")
+    public String health(){return "<h1>OK</h1>";}
+}
